9b24fef94dcf5d571ab06d407f91e57fb2c64a91
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-25 17:58:49 +0300

    Add project files.

---
+++ CSharp16/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> elemanlar = new List<int>();
+elemanlar.Add(0);
+elemanlar.Add(1);
+elemanlar.Add(2);
+elemanlar.Add(3);
+elemanlar.Add(4);
+foreach (int eleman in elemanlar)
+{
+    Console.WriteLine(eleman);
+}
+
+Console.WriteLine("Kümenin eleman sayısı : " + elemanlar.Count);
